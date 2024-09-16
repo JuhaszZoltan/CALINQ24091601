11e9ff98683c8b41d6846d70cfb685bb5ee57342
Juhász Zoltán <JuhaszZoltanMate@yahoo.com>
2024-09-16 08:34:06 +0200

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CALINQ24091601
+{
+    internal class Pet
+    {
+        public int Age => (int)(DateTime.Now - BirthDate).TotalDays / 365;
+        public DateTime BirthDate { get; set; }
+        public string Name { get; set; }
+        public bool Sex { get; set; }
+        public string Species { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using CALINQ24091601;
+
+List<Pet> pets = [
+    new Pet() //01
+    {
+        Name = "Wick",
+        Species = "Hamster",
+        BirthDate = new(2022, 05, 03),
+        Sex = true,
+    },
+    new Pet() //02
+    {
+        Name = "Dani",
+        Species = "Parrot",
+        BirthDate = new(2014, 06, 10),
+        Sex = true,
+    },
+    new Pet() //03
+    {
+        Name = "Szőke Sanyi",
+        Species = "Chicken",
+        BirthDate = new(2024, 03, 01),
+        Sex = false,
+    },
+    new Pet() //04
+    {
+        Name = "Nudli",
+        Species = "Cat",
+        BirthDate = new(2021, 08, 10),
+        Sex = true,
+    },
+    new Pet() //05
+    {
+        Name = "Gombóc",
+        Species = "Dog",
+        BirthDate = new(2021, 09, 13),
+        Sex = true,
+    },
+    new Pet() //06
+    {
+        Name = "Snorlax",
+        Species = "Cat",
+        BirthDate = new(2024, 05, 20),
+        Sex = true,
+    },
+    new Pet() //07
+    {
+        Name = "Lajos",
+        Species = "Goat",
+        BirthDate = new(2022, 06, 21),
+        Sex = true,
+    },
+    new Pet() //08
+    {
+        Name = "Gábor",
+        Species = "Chicken",
+        BirthDate = new(2024, 01, 03),
+        Sex = false,
+    },
+    new Pet() //09
+    {
+        Name = "Rex",
+        Species = "Dog",
+        BirthDate = new(2019, 07, 11),
+        Sex = true,
+    },
+];
